cb6278e43a489e7a2e53594f6d2bee7dd98902f1
Liang Geng <geng.161@osu.edu>
2023-09-05 10:26:50 -0400

    codebase

---
+++ src/test/java/org/osuswe/mdc/MdcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.osuswe.mdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
